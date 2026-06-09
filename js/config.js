bf71ae076b6288fda030a137317e453790569bdc
hsinkr <hsinkr@gmail.com>
2026-06-10 08:31:29 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window.WALKGOO_CONFIG = {
   // 한국관광공사 TourAPI 서비스키
   // 중요: data.go.kr에서 받은 Decoding 키를 권장합니다.
   // 이미 %2F, %3D 같은 문자가 들어있는 Encoding 키도 자동 처리합니다.
-  TOUR_API_KEY: 'EHUKk9PAVNzpXikgwOdKVxbYhgfzW8SLrkVLV8KhaKOk29bX9hDaGMwXjQWEnarNUejsgLD9T2yrBLdsCH2KTA%3D%3D',
+  TOUR_API_KEY: 'EHUKk9PAVNzpXikgwOdKVxbYhgfzW8SLrkVLV8KhaKOk29bX9hDaGMwXjQWEnarNUejsgLD9T2yrBLdsCH2KTA==',
   TOUR_API_BASE: 'https://apis.data.go.kr/B551011/KorService2',
 
   KAKAO_JS_KEY: '',

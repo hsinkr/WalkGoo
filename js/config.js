da70a519ca92c7a37ed4c03f40444a43ba254157
dodogoo <hsinkr@gmail.com>
2026-06-10 10:35:40 +0900

    Add WalkGoo cache actions structure

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window.WALKGOO_CONFIG = {
   // 브라우저에서 직접 API를 호출하지 않고 data/merged/walkgoo_places.json을 우선 사용합니다.
   // GitHub Actions 또는 scripts/update-cache.mjs에서 최신 데이터를 생성하는 구조입니다.
-  TOUR_API_KEY: '',
-  DURUNUBI_API_KEY: '',
+  TOUR_API_KEY: 'EHUKk9PAVNzpXikgwOdKVxbYhgfzW8SLrkVLV8KhaKOk29bX9hDaGMwXjQWEnarNUejsgLD9T2yrBLdsCH2KTA==',
+  DURUNUBI_API_KEY: 'EHUKk9PAVNzpXikgwOdKVxbYhgfzW8SLrkVLV8KhaKOk29bX9hDaGMwXjQWEnarNUejsgLD9T2yrBLdsCH2KTA==',
   TOUR_API_BASE: 'https://apis.data.go.kr/B551011/KorService2',
   DURUNUBI_API_BASE: 'https://apis.data.go.kr/B551011/DurunubiService',
 
